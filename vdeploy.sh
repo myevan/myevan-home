dd1eb041c6da044a573f4c7a4adf5f50bf138eba
myevan <myevan@outlook.com>
2022-11-27 02:27:29 +0000

    site git clone 실패로 인해 배포 안 돠는 버그 수정

--- vdeploy.sh
+++ vdeploy.sh
@@ -1,7 +1,12 @@
 THIS_DIR=$(dirname $0)
 
-if [ ! -d site ]; then
-    git clone git@github.com:myevan/myevan.github.io site
+if [ ! -d $THIS_DIR/site ]; then
+    git clone git@github.com:myevan/myevan.github.io $THIS_DIR/site
+fi
+
+if [ ! -d $THIS_DIR/site/.git ]; then
+    echo NOT_BOUND_SITE.GIT
+    exit
 fi
 
 $THIS_DIR/vmkdocs.sh build
